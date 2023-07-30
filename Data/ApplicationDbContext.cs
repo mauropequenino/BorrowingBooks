b13ff877d4c36e3c17cc513ea79bb12694c07f9c
Mauro E. Pequenino <73997145+mauropequenino@users.noreply.github.com>
2023-07-30 22:50:10 +0200

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BorrowingBooks.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BorrowingBooks.Data
+{
+    //Ajuda a conectar e realizar operacoes com  banco de dados
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Criar tabela
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
+    }
+}
